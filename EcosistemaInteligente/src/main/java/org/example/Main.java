9df45b04270a79eac0eb46d177e1adfef696c1a3
KravAD <dkrav@myuax.com>
2024-03-17 22:34:36 +0100

    commit 14
    instacias en el main

--- EcosistemaInteligente/src/main/java/org/example/Main.java
+++ EcosistemaInteligente/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import ModeladoIdentidades.Ambiente;
+import ModeladoIdentidades.Animal;
+import ModeladoIdentidades.Organismo;
+import ModeladoIdentidades.Planta;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Organismo leon = new Animal(1, 1, 100, 5, true, "mamifero");
+        Organismo baobab = new Planta(2, 2, 100, 15, true, "arbol");
+        Ambiente sabana = new Ambiente("calido", "pasto", 100);
+
+        
+
     }
 }
