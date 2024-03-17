887d1a22a6df64e8c828cb0991fcd880cc229d3f
KravAD <dkrav@myuax.com>
2024-03-17 22:11:39 +0100

    commit 8
    constructor de clase ambiente.

--- EcosistemaInteligente/src/main/java/ModeladoIdentidades/Ambiente.java
+++ EcosistemaInteligente/src/main/java/ModeladoIdentidades/Ambiente.java
@@ -4,5 +4,10 @@ public class Ambiente {
     protected String clima;
     protected String terreno;
     protected int recursosDisponibles;
-    
+
+    public Ambiente(String clima, String terreno, int recursosDisponibles) {
+        this.clima = clima;
+        this.terreno = terreno;
+        this.recursosDisponibles = recursosDisponibles;
+    }
 }
