5a1f16c849c34a0010301e5c9f80268f2f32cba7
KravAD <dkrav@myuax.com>
2024-03-17 21:55:39 +0100

    commit 3
    atributos del organismo

--- EcosistemaInteligente/src/main/java/ModeladoIdentidades/organismo.java
+++ EcosistemaInteligente/src/main/java/ModeladoIdentidades/organismo.java
@@ -1,4 +1,9 @@
 package ModeladoIdentidades;
 
 public class organismo {
+    protected int posicionX;
+    protected int posicionY;
+    protected int salud;
+    protected int edad;
+    protected boolean estadoReproductivo;
 }
