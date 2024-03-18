ee15707c81c5ba77c3b93c5a987d7232f3b358b8
KravAD <dkrav@myuax.com>
2024-03-18 21:00:18 +0100

    commit 32
    constructor

--- EcosistemaInteligente/src/main/java/UsuariosSimulaciones/Usuario.java
+++ EcosistemaInteligente/src/main/java/UsuariosSimulaciones/Usuario.java
@@ -3,4 +3,9 @@ package UsuariosSimulaciones;
 public class Usuario {
     protected String usuario;
     protected String contraseña;
+
+    public Usuario(String usuario, String contraseña) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
 }
