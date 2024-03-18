71f89c279e25636d90c96a43e2622e2a06ad432c
KravAD <dkrav@myuax.com>
2024-03-18 21:37:24 +0100

    commit 41
    método para registrar

--- EcosistemaInteligente/src/main/java/UsuariosSimulaciones/Registrador.java
+++ EcosistemaInteligente/src/main/java/UsuariosSimulaciones/Registrador.java
@@ -1,9 +1,20 @@
 package UsuariosSimulaciones;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Registrador {
     protected String nombreArchivo;
 
     public Registrador(String nombreArchivo) {
         this.nombreArchivo = nombreArchivo;
+    }
+
+    public void registrar(String mensaje) {
+        try (FileWriter escritor = new FileWriter(nombreArchivo, true)) {
+            escritor.write(mensaje + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
