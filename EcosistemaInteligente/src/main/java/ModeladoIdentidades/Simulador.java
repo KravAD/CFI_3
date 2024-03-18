66de5671003674efa090a5ec38aa5b70cc2cc9b8
KravAD <dkrav@myuax.com>
2024-03-18 20:09:11 +0100

    commit 29
    logica de los metodos

--- EcosistemaInteligente/src/main/java/ModeladoIdentidades/Simulador.java
+++ EcosistemaInteligente/src/main/java/ModeladoIdentidades/Simulador.java
@@ -9,12 +9,33 @@ public class Simulador {
         this.ambiente = ambiente;
     }
     public void simular() {
-        }
+        Organismo[] organismosArray = organismos.toArray(new Organismo[0]);
+        ambiente.simularInteracciones(organismosArray);
+        ambiente.simularEventosAleatorios(organismosArray);    }
 
     public void calcularEstadisticas() {
+        int totalSalud = 0;
+        int totalEdad = 0;
+        int totalReproductivos = 0;
 
+        for (Organismo organismo : organismos) {
+            totalSalud += organismo.getSalud();
+            totalEdad += organismo.getEdad();
+            if (organismo.isEstadoReproductivo()) {
+                totalReproductivos++;
+            }
+        }
+
+        double promedioSalud = (double) totalSalud / organismos.size();
+        double promedioEdad = (double) totalEdad / organismos.size();
+        double porcentajeReproductivos = (double) totalReproductivos / organismos.size() * 100;
+
+        System.out.println("Promedio de salud: " + promedioSalud);
+        System.out.println("Promedio de edad: " + promedioEdad);
+        System.out.println("Porcentaje de organismos en estado reproductivo: " + porcentajeReproductivos + "%");
     }
 
     public void presentarEstadisticas() {
+        calcularEstadisticas();
     }
 }
