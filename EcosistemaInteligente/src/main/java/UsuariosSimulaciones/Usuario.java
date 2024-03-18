22135ea28ee74794930acb5d71dc9b30c3d9df37
KravAD <dkrav@myuax.com>
2024-03-18 21:00:52 +0100

    commit 33
    getters y setters

--- EcosistemaInteligente/src/main/java/UsuariosSimulaciones/Usuario.java
+++ EcosistemaInteligente/src/main/java/UsuariosSimulaciones/Usuario.java
@@ -8,4 +8,20 @@ public class Usuario {
         this.usuario = usuario;
         this.contraseña = contraseña;
     }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 }
