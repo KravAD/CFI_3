ea345fb8e78043f638b05b55ad640bbc1a11711f
KravAD <dkrav@myuax.com>
2024-03-18 20:02:22 +0100

    commit 27
    constructor

--- EcosistemaInteligente/src/main/java/ModeladoIdentidades/Simulador.java
+++ EcosistemaInteligente/src/main/java/ModeladoIdentidades/Simulador.java
@@ -4,4 +4,8 @@ import java.util.List;
 public class Simulador {
     private List<Organismo> organismos;
     private Ambiente ambiente;
+    public Simulador(List<Organismo> organismos, Ambiente ambiente) {
+        this.organismos = organismos;
+        this.ambiente = ambiente;
+    }
 }
