5a6ddb7cd88a4f064424bd043a56c36e030beaad
KravAD <dkrav@myuax.com>
2024-03-18 21:33:15 +0100

    commit 40
    constructor

--- EcosistemaInteligente/src/main/java/UsuariosSimulaciones/Registrador.java
+++ EcosistemaInteligente/src/main/java/UsuariosSimulaciones/Registrador.java
@@ -2,4 +2,8 @@ package UsuariosSimulaciones;
 
 public class Registrador {
     protected String nombreArchivo;
+
+    public Registrador(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
 }
