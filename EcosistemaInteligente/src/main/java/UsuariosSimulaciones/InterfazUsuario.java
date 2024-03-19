2011ff442d96c6453d99d32cc7fc9bb516cd21d7
KravAD <dkrav@myuax.com>
2024-03-19 18:40:37 +0100

    commit 45
    constructor

--- EcosistemaInteligente/src/main/java/UsuariosSimulaciones/InterfazUsuario.java
+++ EcosistemaInteligente/src/main/java/UsuariosSimulaciones/InterfazUsuario.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class InterfazUsuario {
     private Scanner scanner;
 
-    
+
+    public InterfazUsuario() {
+        this.scanner = new Scanner(System.in);
+    }
 
 }
